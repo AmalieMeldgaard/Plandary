c084f77558c923c4e6053fe1a464bd3b6370d2f0
nomigayatri <nomigayatri.gruenewald@gmail.com>
2021-05-06 11:55:23 +0200

    js buttons

--- dev/welcome/script.js
+++ dev/welcome/script.js
@@ -1,5 +1,19 @@
 // sign in button
-document.querySelector('.navigateToSignIn')
+document.querySelector('.welcome_signin')
   .addEventListener('click', function () {
-  window.location.href = 'signIn.html';
+  window.location.href = '../login/signIn.html';
 });
+
+// signin to home
+document.querySelector('.signin')
+.addEventListener('click', function () {
+window.location.href = '../home/home.html';
+});
+
+
+// home to scan
+document.querySelector('.scan__img')
+  .addEventListener('click', function () {
+  window.location.href = '..scan/scan.html';
+});
+
