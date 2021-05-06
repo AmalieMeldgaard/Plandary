cf0450db150dde2e33161a46cc3c63dcb517634f
AmalieMeldgaard <70745423+AmalieMeldgaard@users.noreply.github.com>
2021-05-06 12:34:41 +0200

    welcome

--- dev/welcome/script.js
+++ dev/welcome/script.js
@@ -1,19 +1,16 @@
 // sign in button
-document.querySelector('.welcome_signin')
-  .addEventListener('click', function () {
-  window.location.href = '../login/signIn.html';
-});
+document
+  .querySelector(".welcome_signin")
+  .addEventListener("click", function () {
+    window.location.href = "../login/signIn.html";
+  });
 
 // signin to home
-document.querySelector('.signin')
-.addEventListener('click', function () {
-window.location.href = '../home/home.html';
+document.querySelector(".signin").addEventListener("click", function () {
+  window.location.href = "../home/home.html";
 });
-
 
 // home to scan
-document.querySelector('.scan__img')
-  .addEventListener('click', function () {
-  window.location.href = '..scan/scan.html';
+document.querySelector(".scan__img").addEventListener("click", function () {
+  window.location.href = "..scan/scan.html";
 });
-
